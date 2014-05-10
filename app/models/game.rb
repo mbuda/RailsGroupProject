c9f485775a62a4223a4607e665b490a074f9e33e
Marta Buda <bmarta1991@o2.pl>
2014-05-10 13:04:32 +0200

    [#3]add game.rb

---
+++ app/models/game.rb
@@ -0,0 +1,11 @@
+class Game < ActiveRecord::Base
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :release_date, presence: true
+  validates :requirements, presence: true
+  validates :genre, presence: true
+  validates :PEGI, presence: true
+  validates :producent, presence: true
+
+end
