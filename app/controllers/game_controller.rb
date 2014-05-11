c39fd203e22758f0dc1c973e0b57402da6e25e44
aczerwinska <czerwinska.agnieszkalucja@gmail.com>
2014-05-11 18:04:44 +0200

    15-vieWNewGame

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,2 +1,9 @@
 class GamesController < ApplicationController
+
+def new
+end
+
+def show
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Group::Application.routes.draw do
+
+resources :games
+ root to: 'games#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
