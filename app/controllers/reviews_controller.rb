50ff43b0945ed1d493d3fabdc633258b89bd6af5
Marta Buda <bmarta1991@o2.pl>
2014-05-12 19:32:47 +0200

    [#10]add new review view
    [#10]minor fixes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,25 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+      if @review.save
+        redirect_to @review, notice: "Saved review"
+      else
+        render action: 'new'
+      end
+  end
+  
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :description)
+  end
+
 end
