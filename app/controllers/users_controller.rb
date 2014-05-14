2c8c9c09f05cb1ebe0c5e088891cbb7afde7aaaf
Michał Maciejewski <michal.maciejewski92@gmail.com>
2014-05-14 16:41:18 +0200

    user-controll-create-add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,13 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+    	   @user = User.new(user_params)
+             if @user.save
+        	redirect_to @user, notice: "Saved user"
+      		else
+        	render action: 'new'
+           end
+        end
+
 end
