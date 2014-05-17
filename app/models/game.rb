8143bcf2cb5380ebe9d7f2a8a6392e3f3e86b7a9
Michał Maciejewski <michal.maciejewski92@gmail.com>
2014-05-17 13:33:18 +0200

    39-added-nil-security

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,4 +10,40 @@ class Game < ActiveRecord::Base
   validates :PEGI, presence: true
   validates :producent, presence: true
 
+  def title_capital
+    	title.capitalize unless title.nil?
+  end
+
+  def full_description
+    	description.split.join(' ') unless description.nil?
+  end
+
+  def min_description
+    	description.split[0..15].join(' ') unless description.nil?
+  end
+
+  def game_title
+    	game.title unless title.nil?
+  end
+
+  def rel_date
+    	game.release_date unless release_date.nil? 
+  end
+
+  def req
+    	game.requirements unless requirements.nil?
+  end
+
+  def game_genre
+	geme.genre unless genre.nil?
+  end
+
+  def game_PEGI
+	game.PEGI unless PEGI.nil?
+  end
+
+  def game_producent
+	game.producent unless producent.nil?
+  end
+
 end
