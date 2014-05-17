c9f82045e5b800ea7ab2b944a44396ef5e449173
Marta Buda <bmarta1991@o2.pl>
2014-05-17 21:19:40 +0200

    [#40]modified review controller and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,55 +1,60 @@
 class ReviewsController < ApplicationController
-  before_filter :set_review, :only => [:show, :edit, :update, :destroy]
+  before_action :game
 
   def index
     @reviews = Review.all
   end
 
   def show
+    @review = @game.reviews.find(params[:id])
   end
 
   def new
-    @review = Review.new
+    @review = @game.reviews.build
   end
 
   def create
-    @review = Review.new(review_params)
-      if @review.save
-        redirect_to @review, notice: "Saved review"
-      else
-        render action: 'new'
-      end
+    @review = @game.reviews.new(review_params)
+    if @review.save
+      redirect_to [@game, @review], notice: "Saved review"
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @review = @game.reviews.find(params[:id])
   end
 
   def update
-    respond_to do |format|
-      if @review.update_attributes(params[:post])
-        flash[:notice] = 'Review successfully updated'
-        format.html { redirect_to(@review) }
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = 'Review successfully updated'
+      respond_to do |format|
+        format.html { redirect_to game_review_path(@game, @review) }
         format.xml { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml { render :xml => @review.errors,
-                     :status => :unprocessable_entity }
       end
+    else
+      format.html { render :action => "edit" }
+      format.xml { render :xml => @review.errors,
+        :status => :unprocessable_entity }
     end
   end
 
   def destroy
-    @review.destroy
-
+    Review.find(params[:id]).destroy
     respond_to do |format|
-      format.html { redirect_to(game_reviews_path) }
+      format.html { redirect_to game_reviews_path }
       format.xml { head :ok }
     end
   end
 
-  def set_review
-    @review = Review.find(params[:id])
-  end
-
+  private
   def review_params
     params.require(:review).permit(:title, :description)
   end
 
+  def game
+    @game = Game.find(params[:game_id])
+  end
 end
