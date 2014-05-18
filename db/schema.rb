5852e44a1e12ba13ee8fd47f85cc8d779135bf6f
Marta Buda <bmarta1991@o2.pl>
2014-05-18 09:58:20 +0200

    [#43]add relation user-comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510145842) do
+ActiveRecord::Schema.define(version: 20140518075701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140510145842) do
   create_table "comments", force: true do |t|
     t.string  "text"
     t.integer "review_id"
+    t.integer "user_id"
   end
 
   create_table "games", force: true do |t|
